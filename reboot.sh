e2712c25f93407c2175ae66b0f373f4354c9dc73
Da Xue <da.xue@libretech.co>
2022-08-05 13:38:29 -0400

    sleep and then check taint

--- reboot.sh
+++ reboot.sh
@@ -4,11 +4,11 @@ cd $(readlink -f $(dirname ${BASH_SOURCE[0]}))
 
 set -ex
 
+sleep 25
 tainted=$(cat /proc/sys/kernel/tainted)
 if [ $tainted -ne 0 -a $tainted -ne 1024 ]; then
 	exit
 fi
-sleep 25
 loggedin=$(who)
 if [ ! -z "$loggedin" ]; then
 	exit
